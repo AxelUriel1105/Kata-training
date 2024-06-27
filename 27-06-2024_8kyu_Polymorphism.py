cc22f0d87313409a2ae6b37b93510cfe48d0b161
Axel <axeluriel15414@gmail.com>
2024-06-27 14:22:43 -0600

    Este programa sobreescribe el método de la superclase, es decir, se usa polimorfismo

---
+++ 27-06-2024_8kyu_Polymorphism.py
@@ -0,0 +1,11 @@
+"""Task
+Your task is to complete the Cat class which Extends Animal and replace the speak method 
+to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
+
+Reference: JS, Ruby, Python."""
+from preloaded import Animal
+
+class Cat(Animal):
+    def speak(self):
+        return f'{self.name} meows.'
