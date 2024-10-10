4b7aa7497bea0b366d32ecbcdab5a06fc9915a15
Axel <axeluriel15414@gmail.com>
2024-10-09 19:51:54 -0600

    retorna una secuencia de números

---
+++ 09-10-2024_7kyu_arithmetic-sequence-elements.py
@@ -0,0 +1,10 @@
+"""In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+
+The result should be a string of numbers, separated by comma and space.
+
+Example
+# first element: 1, difference: 2, how many: 5
+arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+"""
+def arithmetic_sequence_elements(a, d, n):
+    return ', '.join(str(a+d*n1) for n1 in range(n))
