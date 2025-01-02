9d2593a431d96da8cec4e11ef296861f55205bfd
Axel <axeluriel15414@gmail.com>
2025-01-02 09:42:19 -0600

    filters odd names

---
+++ 2025/02-01-2024_6kyu_find-odd-names.py
@@ -0,0 +1,17 @@
+"""You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
+  { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+];
+write a function that when executed as findOddNames(list1) returns only the developers where if you add the ASCII representation of all characters in their first names, the result will be an odd number:
+
+[
+  { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+]"""
+def find_odd_names(lst): 
+    return [d for d in lst if ASCII_sum(d.get('firstName'))%2!=0]
+def ASCII_sum(name):
+    return sum(ord(l) for l in name)
