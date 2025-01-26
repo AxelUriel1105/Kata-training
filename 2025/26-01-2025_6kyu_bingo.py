be0295045cfa94bd88706ada89c2ed040c134c25
Axel <axl.uriel15414@gmail.com>
2025-01-26 14:16:57 -0600

    win or lose the lottery

---
+++ 2025/26-01-2025_6kyu_bingo.py
@@ -0,0 +1,17 @@
+"""Time to win the lottery!
+
+Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+Example ticket:
+
+[ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+
+Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+"""
+def bingo(ticket,win):
+    mini_wins = [1 for pair in ticket if chr(pair[1]) in pair[0]]
+    return ('Loser!','Winner!')[sum(mini_wins)>= win]
