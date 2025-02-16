ac54efe2b4b71df0fc9a66931d5aadb294797a2a
Axel <axl.uriel15414@gmail.com>
2025-02-15 21:38:35 -0600

    removes words with one exclamation mark

---
+++ 2025/15-02-2025_7kyu_remove.py
@@ -0,0 +1,14 @@
+"""Description:
+Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+
+Examples
+remove("Hi!") === ""
+remove("Hi! Hi!") === ""
+remove("Hi! Hi! Hi!") === ""
+remove("Hi Hi! Hi!") === "Hi"
+remove("Hi! !Hi Hi!") === ""
+remove("Hi! Hi!! Hi!") === "Hi!!"
+remove("Hi! !Hi! Hi!") === "!Hi!"
+"""
+def remove(s):
+    return ' '.join(word for word in s.split() if not word.count('!')==1)
