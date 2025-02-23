1b5ca86cbc811cfffc231d8c271171426dce890e
Axel <axl.uriel15414@gmail.com>
2025-02-23 17:08:28 -0600

    return True if exactly 2 of the integers are positive

---
+++ 2025/23-02-2025_7kyu_two-are-positive.py
@@ -0,0 +1,12 @@
+"""Task:
+Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of the three integers are positive numbers (greater than zero), and False - otherwise.
+
+Examples:
+two_are_positive(2, 4, -3) == True
+two_are_positive(-4, 6, 8) == True
+two_are_positive(4, -6, 9) == True
+two_are_positive(-4, 6, 0) == False
+two_are_positive(4, 6, 10) == False
+two_are_positive(-14, -3, -4) == False"""
+def two_are_positive(a, b, c):
+    return len([n for n in (a,b,c) if n > 0]) == 2
