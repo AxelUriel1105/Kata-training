55376e7fbc0a372a0a8fe8cd58601be8ed2d5f84
Axel <axl.uriel15414@gmail.com>
2025-03-20 17:26:23 -0600

    extracts file name with extension

---
+++ 2025/20-03-2025_6kyu_extract-file-name.py
@@ -0,0 +1,31 @@
+"""You have to extract a portion of the file name as follows:
+
+Assume it will start with date represented as long number
+Followed by an underscore
+You'll have then a filename with an extension
+it will always have an extra extension at the end
+Inputs:
+1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION
+
+1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34
+
+1231231223123131_myFile.tar.gz2
+Outputs
+FILE_NAME.EXTENSION
+
+This_is_an_otherExample.mpg
+
+myFile.tar
+Acceptable characters for random tests:
+
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-0123456789
+
+The recommended way to solve it is using RegEx and specifically groups."""
+import re
+class FileNameExtractor:
+    @staticmethod
+    def extract_file_name(dirty_file_name):
+        print(dirty_file_name)
+        pattern = re.compile(r'(\d+)_([\w-]+)\.([\w-]+)\.([\w-]+)')
+        matches = pattern.findall(dirty_file_name)
+        return matches[0][1] + '.' + matches[0][2]
