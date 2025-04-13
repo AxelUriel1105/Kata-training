48f357152014625e7b18c388f9844a073e30529d
Axel <axl.uriel15414@gmail.com>
2025-04-13 10:57:00 -0600

    same order

---
+++ 2025/13-04-2025_6kyu_name-in-str.py
@@ -0,0 +1,52 @@
+"""What's in a name?
+...Or rather, what's a name in? For us, a particular string is where we are looking for a name.
+
+Task
+Write a function, taking two strings in parameter, that tests whether or not the first string contains all of the letters of the second string, in order.
+
+The function should return true if that is the case, and else false. Letter comparison should be case-INsensitive.
+
+Examples
+    "Across the rivers", "chris" --> true
+      ^      ^  ^^   ^
+      c      h  ri   s
+                
+    Contains all of the letters in "chris", in order.
+----------------------------------------------------------
+    "Next to a lake", "chris" --> false
+
+    Contains none of the letters in "chris".
+--------------------------------------------------------------------
+    "Under a sea", "chris" --> false
+         ^   ^
+         r   s
+
+    Contains only some of the letters in "chris".
+--------------------------------------------------------------------
+    "A crew that boards the ship", "chris" --> false
+       cr    h              s i
+       cr                h  s i  
+       c     h      r       s i
+                 ...
+          
+    Contains all of the letters in "chris", but not in order.
+--------------------------------------------------------------------
+    "A live son", "Allison" --> false
+     ^ ^^   ^^^
+     A li   son
+            
+    Contains all of the correct letters in "Allison", in order, but not enough of all of them (missing an 'l').
+"""
+def name_in_str(strng : str, name : str) -> bool:
+    print(strng,name)
+    strng = strng.lower()
+    name = name.lower()
+    if not set(name).issubset(set(strng)): return False
+    index = 0 
+    for i in name:
+        if i in strng:
+            strng = strng[strng.index(i)+1:]
+        else:
+            return False
+    return True
+            
