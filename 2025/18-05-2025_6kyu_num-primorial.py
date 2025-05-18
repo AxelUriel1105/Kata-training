d294b5fd0ae39be5e26c14c4bec6b8741fcab191
Axel <axl.uriel15414@gmail.com>
2025-05-18 12:29:10 -0600

    return the primordial of a number

---
+++ 2025/18-05-2025_6kyu_num-primorial.py
@@ -0,0 +1,48 @@
+"""Definition (Primorial Of a Number)
+Is similar to factorial of a number, In primorial, not all the natural numbers get multiplied, only prime numbers are multiplied to calculate the primorial of a number. It's denoted with P# and it is the product of the first n prime numbers.
+
+Task
+Given a number N , calculate its primorial.
+
+Notes
+Only positive numbers will be passed (N > 0) .
+Input >> Output Examples:
+1- numPrimorial (3) ==> return (30)
+Explanation:
+Since the passed number is (3) ,Then the primorial should obtained by multiplying 2 * 3 * 5 = 30 .
+
+Mathematically written as , P3# = 30 .
+
+2- numPrimorial (5) ==> return (2310)
+Explanation:
+Since the passed number is (5) ,Then the primorial should obtained by multiplying  2 * 3 * 5 * 7 * 11 = 2310 .
+
+Mathematically written as , P5# = 2310 .
+
+3- numPrimorial (6) ==> return (30030)
+Explanation:
+Since the passed number is (6) ,Then the primorial should obtained by multiplying  2 * 3 * 5 * 7 * 11 * 13 = 30030 .
+
+Mathematically written as , P6# = 30030 .
+
+"""
+import numpy as np
+import math
+def num_primorial(n):
+    prime_numbers, num = [], 2
+    while len(prime_numbers) < n:
+        if is_prime(num):
+            prime_numbers.append(num)
+        num+=1
+    result = 1
+    for p in prime_numbers:
+        result*=p
+    return result
+    return 
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
