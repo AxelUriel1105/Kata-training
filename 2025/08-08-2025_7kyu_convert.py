a3b282379c82b2d128af0a7203bd603e33e9bdde
Axel <axl.uriel15414@gmail.com>
2025-08-08 10:53:52 -0600

    Chore: Solve 7kyu kata - convert a number into a string

---
+++ 2025/08-08-2025_7kyu_convert.py
@@ -0,0 +1,14 @@
+"""You have to create a function that converts integer given as string into ASCII uppercase letters or spaces.
+
+All ASCII characters have their numerical order in table.
+
+For example,
+
+from ASCII table, character of number 65 is "A".
+Numbers will be next to each other, So you have to split given number to two digit long integers.
+
+Examples (input -> output)
+'658776' -> 'AWL' (because in ASCII table 'A' is 65, 'W' is 87, 'L' is 76)
+'73327673756932858080698267658369' ->'I LIKE UPPERCASE'"""
+def convert(number):
+    return ''.join(chr(int(str(number)[i-2:i])) for i in range(2,len(number)+1,2))
