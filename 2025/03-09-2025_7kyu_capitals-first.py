40e0f03b0cdbbdc9d83d955867252a1b91ee4709
Axel <axl.uriel15414@gmail.com>
2025-09-03 09:00:19 -0600

    Chore: Solve 7kyu kata - sort a string, such that uppercase words go first

---
+++ 2025/03-09-2025_7kyu_capitals-first.py
@@ -0,0 +1,13 @@
+"""Create a function that takes an input String and returns a String, where all the uppercase words of the input String are in front and all the lowercase words at the end. The order of the uppercase and lowercase words should be the order in which they occur.
+
+If a word starts with a number or special character, skip the word and leave it out of the result.
+
+Input String will not be empty.
+
+For an input String: "hey You, Sort me Already!" the function should return: "You, Sort Already! hey me"
+
+"""
+def capitals_first(text):
+    upper_words = ' '.join(w for w in text.split() if w[0].isupper())
+    lower_words = ' '.join(w for w in text.split() if w[0].islower())
+    return (upper_words + ' ' + lower_words).strip()
