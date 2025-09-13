c992a7e7bb1c5af7a48b7f513b520f1db71f08a0
Axel <axl.uriel15414@gmail.com>
2025-09-13 12:13:06 -0600

    Chore: Solve 7kyu kata - get the decimal part

---
+++ 2025/13-09-2025_7kyu_get-decimal.py
@@ -0,0 +1,10 @@
+"""Write a function that returns only the decimal part of the given number.
+
+You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+
+Examples
+get_decimal(2.4)  # 0.4
+get_decimal(-0.2) # 0.2"""
+def get_decimal(n): 
+    float_n = str(float(n))
+    return float(float_n[float_n.find('.'):])
