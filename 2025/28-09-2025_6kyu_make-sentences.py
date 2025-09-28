ccde0dec48725ba773b62df1886935398f73bdd9
Axel <axl.uriel15414@gmail.com>
2025-09-28 08:12:43 -0600

    Chore: Solve 6kyu kata - join a sentence with certain conditions

---
+++ 2025/28-09-2025_6kyu_make-sentences.py
@@ -0,0 +1,19 @@
+"""Implement a function, so it will produce a sentence out of the given parts.
+
+Array of parts could contain:
+
+words;
+commas in the middle;
+multiple periods at the end.
+Sentence making rules:
+
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+Example:
+
+makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.'
+"""
+def make_sentences(parts):
+    join_sentence = ' '.join(parts).replace(' ,',',').replace('.','')
+    return join_sentence.strip()+'.'
